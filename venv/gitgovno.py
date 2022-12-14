1bb4005e26d29aa531fa8a9bf70f6ed01fdcba12
makse <maks.esipov.02@mail.ru>
2022-12-14 23:10:01 +0300

    jopa nerga x2

--- venv/gitgovno.py
+++ venv/gitgovno.py
@@ -1,6 +1,13 @@
-import selenium
-print("hello niger")
-print("burgeking")
-print("ass")
-print("jopanerga")
-print("huila")
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+driver = webdriver.Chrome(executable_path="D:\\SelDrivers\\chromedriver.exe")
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element_by_name("q")
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+driver.close()
