cc48abf1a81cadccd2b829b26134deff8e0c9f97
Cucuruz <z2013zz@bk.ru>
2022-12-15 12:00:31 +0300

    fixed bs

--- venv/Beautiful_soup_parser.py
+++ venv/Beautiful_soup_parser.py
@@ -2,18 +2,21 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
-main_url = "https://priem.pgups.ru/doc_passmark.php"
-req = requests.get(main_url)
-soup = BeautifulSoup(req.text, "html.parser")
 
-proh_ball = []
-table = soup.find('table', attrs={'class':'table'})
-table_body = table.find('tbody')
+def proh_ball_func():
+    main_url = "https://priem.pgups.ru/doc_passmark.php"
+    req = requests.get(main_url)
+    soup = BeautifulSoup(req.text, "html.parser")
 
-rows = table_body.find_all('tr')
-for row in rows:
-    cols = row.find_all('td')
-    cols = [ele.text.strip() for ele in cols]
-    proh_ball.append([ele for ele in cols if ele])
+    proh_ball = []
+    table = soup.find('table', attrs={'class':'table'})
+    table_body = table.find('tbody')
+
+    rows = table_body.find_all('tr')
+    for row in rows:
+        cols = row.find_all('td')
+        cols = [ele.text.strip() for ele in cols]
+        proh_ball.append([ele for ele in cols if ele])
+    return proh_ball
 
 
