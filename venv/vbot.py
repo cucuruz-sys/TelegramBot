3bb0c3a8a2cc8875f98d0d024f7704bf9f86305a
makse <maks.esipov.02@mail.ru>
2022-12-15 09:59:13 +0300

    jopa nerga x2

--- venv/vbot.py
+++ venv/vbot.py
@@ -1,61 +1,8 @@
 import vk_api
-from vk_api.longpool import VkLongPoll, VkEventType
 
-token = "YxoDnZ_0tnqd97ikODErQFX1DxvwOGC9nRyTULXCcclGbfONcP91cRB-ozUBKW7CHxETUbch_BuCKrMEihqZWnjItbWUfDgxauBfrOo2DF7j2s_pLEEQsdZUCZSXLLtcoTxbjAk81RGkDpewkGuvlXJoX4tKvnN2wqmqz58rNWQoyZ9WycH0-YMG-JsxCKYhv8p_6uTvx3InZaoMWXRg"
+vk_session = vk_api.VkApi('+71234567890', 'mypassword')
+vk_session.auth()
 
+vk = vk_session.get_api()
 
-def write_msg(user_id, message):
-    vk.method('messages.send', {'user_id': user_id, 'message': message})
-
-
-vk = vk_api.VkApi(token=token)
-
-# Работа с сообщениями
-longpoll = VkLongPoll(vk)
-
-for event in longpoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW:
-        if event.to_me:
-            request = event.text
-            if(request == "Старт"):
-                write_msg(event.user_id, "Добро пожаловать в чат бот приёмной комиссии ПГУПС!")
-                write_msg(event.user_id, "Главный раздел")
-                write_msg(event.user_id, "Направление подготовки и специальности")
-                write_msg(event.user_id, "Необходимые вступительные испытания")
-                write_msg(event.user_id, "Главные даты приёмной комиссии")
-                write_msg(event.user_id, "Минимальные проходные баллы")
-                write_msg(event.user_id, "Дополнительное обучение")
-                write_msg(event.user_id, "Платные образовательные услуги")
-                write_msg(event.user_id, "Об университете")
-                write_msg(event.user_id, "Вопросы и ответы")
-                write_msg(event.user_id, "Контакты")
-                write_msg(event.user_id, "Оставить заявку")
-
-
-                #направления
-                #подготовки
-                #и
-                #специальности;
-                #— необходимые
-                #вступительные
-                #испытания;
-                #— главные
-                #даты
-                #приемной
-                #кампании;
-                #— минимальные
-                #проходные
-                #баллы;
-                #— дополнительное
-                #обучение;
-                #— платные
-                #образовательные
-                #услуги;
-                #— об
-                #университете;
-                #— вопросы
-                #и
-                #ответы;
-                #— контакты;
-                #— оставить
-                #заявку.
+print(vk.wall.post(message='Hello world!'))
